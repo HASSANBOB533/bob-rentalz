7e4cf21292904819a07a428d0415e1e0b4efdcd7
HASSANBOB533 <cs@bestofbedz.com>
2025-11-21 18:27:45 +0400

    Refactor App component to use AppRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,5 @@
-import { Routes, Route } from 'react-router-dom';
+import AppRouter from "./router/AppRouter";
 
-// Dashboard pages (we will create them next)
-import AdminDashboard from './pages/dashboard/AdminDashboard';
-import OwnerDashboard from './pages/dashboard/OwnerDashboard';
-import TenantDashboard from './pages/dashboard/TenantDashboard';
-import AgentDashboard from './pages/dashboard/AgentDashboard';
-
-function App() {
-  return (
-    <Routes>
-
-      {/* Home Page */}
-      <Route path="/" element={<div>Home Page</div>} />
-
-      {/* Dashboards */}
-      <Route path="/dashboard/admin" element={<AdminDashboard />} />
-      <Route path="/dashboard/owner" element={<OwnerDashboard />} />
-      <Route path="/dashboard/tenant" element={<TenantDashboard />} />
-      <Route path="/dashboard/agent" element={<AgentDashboard />} />
-
-    </Routes>
-  );
+export default function App() {
+  return <AppRouter />;
 }
-
-export default App;
