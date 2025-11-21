e5bed9d118704caa129d29e863817276482eb355
HASSANBOB533 <cs@bestofbedz.com>
2025-11-21 17:56:53 +0400

    Create AdminDashboard component

---
+++ src/pages/dashboard/AdminDashboard.tsx
@@ -0,0 +1,10 @@
+function AdminDashboard() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold mb-4">Admin Dashboard</h1>
+      <p className="text-gray-600">Welcome to the Admin Dashboard</p>
+    </div>
+  );
+}
+
+export default AdminDashboard;
